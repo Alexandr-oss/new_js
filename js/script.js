a3877e4c05956dc61d8524305ab6516b6d8732ad
Alex <sasha.babaleu@yandex.ru>
2022-01-17 13:16:24 +0300

    fix: script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-let number = "5";
+"use sctrict";
 
-const rute = number * 5;
+let numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
 
-console.log(rute);
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+    a: "b",
+  },
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-let incr = 10,
-  decr = 10;
+let a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?"),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?");
 
-// incr++;
-// decr--;
-console.log(incr--);
-console.log(decr++);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
